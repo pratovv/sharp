546d48253c29cec2637278913fd4b32ce46d37bd
Timur Pratov <timur.pratovv@gmail.com>
2023-12-13 17:54:15 +0600

    first commit

---
+++ MVCShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿// HomeController.cs
+//
+// @Date: 25/2/2018
+// @Auhtor: Roberth Hansson-Tornéus (Gawee.Narak@gmail.com)
+// @Copyright: ©2018 – Roberth Hansson-Tornéus, All rights reserved.
+//
+
+using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCShop.Models;
+
+
+namespace MVCShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "MyShop Demo";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
